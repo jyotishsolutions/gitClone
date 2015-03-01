13bbc53d4e135a1e168e0873932634bb882f9ab4
mmjyothi <Ishansinghpatel@gmail.com>
2015-03-01 18:53:06 +0000

    Added Test1 file by mmjyothi

---
+++ src/com/clone/Test1.java
@@ -0,0 +1,27 @@
+
+package com.clone;
+/**
+* @author Jyothi
+*
+*/
+public class Test {
+:x	   	/**
+				 * 
+				 	 */
+					 	public Test() {
+								// TODO Auto-generated constructor stub
+									}
+
+										/**
+											 * @param args
+											 	 */
+												 	public static void main(String[] args) {
+															// TODO Auto-generated method stub
+
+																	System.out.println(" This is a first check in from the mmjyothi Sequence 1")
+																			
+																					
+																						}
+
+																						}
+
